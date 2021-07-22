70a95b70cf27debde00425d3912cd7df4cd28aeb
Ddae-D <87243869+Ddae-D@users.noreply.github.com>
2021-07-21 21:40:45 -0500

    Suma, resta y multiplicacion con two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miniCalculadora';
-}
+  title = 'Calculadora';
+  checkProperty=false;
+  disabledProperty=true;
+  statusCheckProperty="deshabilitado";
+  // ************************************
+  num1:number = 0;
+  num2:number = 0;
+  resu:number = 0; 
+  // ************************************
+  habilita(){
+    if(this.disabledProperty==true) {
+      this.disabledProperty=false;
+      this.statusCheckProperty="habilitado";
+    }
+    else{
+      this.disabledProperty=true;
+      this.statusCheckProperty="deshabilitado";
+      this.num1 = 0;
+      this.num2 = 0;
+      this.resu = 0;
+    }
+  }
+  suma(){
+    //alert(event.target);//HTMLInputElement
+    this.resu = ((this.num1) + (this.num2));
+  }
+  resta(){
+    //alert(event.target);//HTMLInputElement
+    this.resu = ((this.num1) - (this.num2));
+  }
+  multiplicacion(){
+    //alert(event.target);//HTMLInputElement
+    this.resu = ((this.num1) * (this.num2));
+  }
+} 
